f6adb612c9e72ccd4740150928b1293a88fe0ed8
Abhi <soni5abhi23@gmail.com>
2023-09-29 15:44:19 +0530

    Chat Context update

--- src/context/ChatContext.jsx
+++ src/context/ChatContext.jsx
@@ -1,8 +1,4 @@
-import React, {
-  createContext,
-  useContext,
-  useReducer,
-} from 'react'
+import React, { createContext, useContext, useReducer } from 'react'
 // import { auth } from '../Firebase/Firebase'
 // import { onAuthStateChanged } from 'firebase/auth'
 import { AuthContext } from './AuthContext'
